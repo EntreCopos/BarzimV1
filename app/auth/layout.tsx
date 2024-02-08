78544d7f9dd4b436a029d315736023cfc37eb77d
Evandro Reichert <evandroreichert07@gmail.com>
2024-02-08 11:36:28 -0300

    ✨ 5.001: feat: implementa cores do figma ao root e ao tailwind.config
    cor errada kaka

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,6 +1,6 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex h-full items-center justify-center bg-slate-barzim">
+    <div className="flex h-full items-center justify-center bg-yellow-barzim">
       {children}
     </div>
   )
