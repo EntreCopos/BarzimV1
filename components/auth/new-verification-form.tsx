1cdf068c62124a255243f39f9a97f75e048712cc
Evandro Reichert <evandroreichert07@gmail.com>
2024-02-09 19:09:00 -0300

    🚚 0.000: chore: organiza paths dos components

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -1,13 +1,13 @@
 'use client'
 
+import { useSearchParams } from 'next/navigation'
 import { useCallback, useEffect, useState } from 'react'
 import { BeatLoader } from 'react-spinners'
-import { useSearchParams } from 'next/navigation'
 
 import { newVerification } from '@/actions/new-verification'
 import { CardWrapper } from '@/components/auth/card-wrapper'
-import { FormError } from '@/components/form-error'
-import { FormSuccess } from '@/components/form-success'
+import { FormError } from '@/components/forms/form-error'
+import { FormSuccess } from '@/components/forms/form-success'
 
 export const NewVerificationForm = () => {
   const [error, setError] = useState<string | undefined>()
--- components/auth/register-form.tsx
+++ components/auth/register-form.tsx
@@ -1,12 +1,15 @@
 'use client'
 
-import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { useState, useTransition } from 'react'
 import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
 
-import { RegisterSchema } from '@/schemas'
-import { Input } from '@/components/ui/input'
+import { register } from '@/actions/register'
+import { CardWrapper } from '@/components/auth/card-wrapper'
+import { FormError } from '@/components/forms/form-error'
+import { FormSuccess } from '@/components/forms/form-success'
+import { Button } from '@/components/ui/button'
 import {
   Form,
   FormControl,
@@ -15,11 +18,8 @@ import {
   FormLabel,
   FormMessage,
 } from '@/components/ui/form'
-import { CardWrapper } from '@/components/auth/card-wrapper'
-import { Button } from '@/components/ui/button'
-import { FormError } from '@/components/form-error'
-import { FormSuccess } from '@/components/form-success'
-import { register } from '@/actions/register'
+import { Input } from '@/components/ui/input'
+import { RegisterSchema } from '@/schemas'
 
 export const RegisterForm = () => {
   const [error, setError] = useState<string | undefined>('')
