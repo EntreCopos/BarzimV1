e78770124ee51dfde2ddc88846062505f9cb4302
Evandro Reichert <evandroreichert07@gmail.com>
2024-02-10 13:44:29 -0300

    ✨ 5.006: feat: adiciona componente CardHorizontalCerveja

--- app/age-verification/page.tsx
+++ app/age-verification/page.tsx
@@ -2,7 +2,7 @@ import { AgeVerificationForm } from '@/components/auth/age-verification-form'
 
 export default async function AgeVerificationPage() {
   return (
-    <div className="flex min-h-['100svh'] w-full place-items-center align-middle">
+    <div className="flex min-h-[100svh] w-full items-center justify-center">
       <AgeVerificationForm />
     </div>
   )
