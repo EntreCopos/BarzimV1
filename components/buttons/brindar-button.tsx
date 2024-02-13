c2c9b6e97f7bd148dc2b2971b3cee66ebaa5b3c1
Evandro Reichert <evandroreichert07@gmail.com>
2024-02-13 19:45:23 -0300

    :bug: 5.018: fix: adiciona prop onClick

--- components/buttons/brindar-button.tsx
+++ components/buttons/brindar-button.tsx
@@ -1,8 +1,8 @@
-import BrindarIcon from './brindar-icon'
+import BrindarIcon from './brindar-icon';
 
-export const BrindarButton: React.FC = () => {
+export const BrindarButton: React.FC<{ onClick: () => void; }> = ({ onClick }) => {
     return (
-        <button className="rounded-full bg-yellow-barzim text-[#141414] font-medium text-sm px-4 flex items-center">
+        <button className="rounded-full bg-yellow-barzim text-[#141414] font-medium text-sm px-4 flex items-center" onClick={onClick}>
             <BrindarIcon />
             <span className='ml-1'>Brindar</span>
         </button>
