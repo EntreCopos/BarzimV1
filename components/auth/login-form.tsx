ea535674f2bd1900d207e267c9ee5cf8ad89dca7
Evandro Reichert <evandroreichert07@gmail.com>
2024-02-13 10:40:22 -0300

    ✨ 5.010: feat: adiciona telas de login - /auth/login

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -8,7 +8,6 @@ import { useForm } from 'react-hook-form'
 import * as z from 'zod'
 
 import { login } from '@/actions/login'
-import { CardWrapper } from '@/components/auth/card-wrapper'
 import { FormError } from '@/components/forms/form-error'
 import { FormSuccess } from '@/components/forms/form-success'
 import { Button } from '@/components/ui/button'
@@ -69,90 +68,78 @@ export const LoginForm = () => {
   }
 
   return (
-    <CardWrapper
-      headerLabel="Suas cervejas, seus amigos. Seu Barzim."
-      backButtonLabel="Ainda não tem seu Barzim? Crie agora mesmo"
-      backButtonHref="/auth/register"
-      showSocial
-    >
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-          <div className="space-y-4">
-            {showTwoFactor && (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <div className="space-y-4">
+          {showTwoFactor && (
+            <FormField
+              control={form.control}
+              name="code"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Código 2FA</FormLabel>
+                  <FormControl className=''>
+                    <Input
+                      {...field}
+                      disabled={isPending}
+                      placeholder="666420"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          )}
+          {!showTwoFactor && (
+            <>
               <FormField
                 control={form.control}
-                name="code"
+                name="email"
                 render={({ field }) => (
                   <FormItem>
-                    <FormLabel>Código 2FA</FormLabel>
-                    <FormControl>
+                    <FormControl className='text-white text-opacity-60 bg-zinc-700 bg-opacity-60 border-black border-2 border-opacity-20 w-full h-fit p-3'>
                       <Input
                         {...field}
                         disabled={isPending}
-                        placeholder="666420"
+                        placeholder="E-mail"
+                        type="email"
                       />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
                 )}
               />
-            )}
-            {!showTwoFactor && (
-              <>
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Email</FormLabel>
-                      <FormControl>
-                        <Input
-                          {...field}
-                          disabled={isPending}
-                          placeholder="zedasilva@ambev.com"
-                          type="email"
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Senha</FormLabel>
-                      <FormControl>
-                        <Input
-                          {...field}
-                          disabled={isPending}
-                          placeholder="******"
-                          type="password"
-                        />
-                      </FormControl>
-                      <Button
-                        size="sm"
-                        variant="link"
-                        asChild
-                        className="px-0 font-normal"
-                      >
-                        <Link href="/auth/reset">Exxqueceu a senha é?</Link>
-                      </Button>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </>
-            )}
-          </div>
-          <FormError message={error || urlError} />
-          <FormSuccess message={success} />
-          <Button disabled={isPending} type="submit" className="w-full">
-            {showTwoFactor ? 'Confirmar' : 'Entrar'}
-          </Button>
-        </form>
-      </Form>
-    </CardWrapper>
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl className='text-white text-opacity-60 bg-zinc-700 bg-opacity-60 border-black border-2 border-opacity-20 w-full h-fit p-3'>
+                      <Input
+                        {...field}
+                        disabled={isPending}
+                        placeholder="Senha"
+                        type="password"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </>
+          )}
+        </div>
+        <FormError message={error || urlError} />
+        <FormSuccess message={success} />
+        <Button disabled={isPending} type="submit" className="w-full h-12 my-3 bg-yellow-barzim bg-opacity-80 font-bold text-black rounded-full hover:bg-opacity-100 hover:bg-[#ecbf4e]">
+          {showTwoFactor ? 'Confirmar' : 'Entrar'}
+        </Button>
+
+        <div className='text-center'>
+          <Link href="/auth/login">Ainda não tem sua conta no Barzim? </Link>
+          <p className='text-yellow-barzim  font-medium text-sm'>Cadastre-se aqui.</p>
+        </div>
+      </form>
+    </Form>
   )
 }
