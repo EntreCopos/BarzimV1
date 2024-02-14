800503aee34659d55b42fb5511f8e1f2299fab57
Lara <larabernspereira@gmail.com>
2024-02-14 19:12:29 -0300

    🍺 5.022 - Comp detalhes da cerveja

--- app/dashboardScreen/page.tsx
+++ app/dashboardScreen/page.tsx
@@ -3,6 +3,7 @@ import NavWrapper from '../../components/dashboard/nav-wrapper/nav-wrapper';
 import styles from "./page.module.css"
 import BeersCarousel from '@/components/dashboard/beers-carousel/beers-carousel';
 import ListFindings from '@/components/dashboard/list-where-you-find/list-where-you-find';
+import DetalhesCerveja from '@/components/lists/detalhes-da-cerveja/detalhes-da-cerveja';
 
 const DashboardPage: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const DashboardPage: React.FC = () => {
       <Carousel/>
       <BeersCarousel/>
       <ListFindings/>
+      <DetalhesCerveja id={1}/>
     </div>
   );
 };
