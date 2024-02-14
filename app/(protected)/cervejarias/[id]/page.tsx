afb39d1304d6aed63ad5efeb378d034ad8bd9cb0
Andre <andrezorek@gmail.com>
2024-02-13 21:36:01 -0300

    :sparkles: 3.005: feat: adicionando interfaces com a DB e consumindo dados em RSC

--- app/(protected)/cervejarias/[id]/page.tsx
+++ app/(protected)/cervejarias/[id]/page.tsx
@@ -1,7 +1,30 @@
+import { getCervejasByCervejaria } from '@/data/cervejas'
+import Link from 'next/link'
+
 export default async function CervejariaByIdPage({
   params,
 }: {
   params: { id: string }
 }) {
-  return <h1>Cervejaria: {params.id}</h1>
+  const cervejariaData = await getCervejasByCervejaria(params.id)
+  console.log(cervejariaData?.nome)
+
+  return (
+    <>
+      <h1>Cervejaria {cervejariaData?.nome}</h1>
+      <ul>
+        {cervejariaData?.CervejaShadow.map((cerveja) => {
+          return (
+            <li key={cerveja.id}>
+              <h2>
+                <Link href={`/cervejas/${cerveja.id}`}>
+                  {cerveja.nomeCerveja}
+                </Link>
+              </h2>
+            </li>
+          )
+        })}
+      </ul>
+    </>
+  )
 }
--- app/(protected)/cervejas/page.tsx
+++ app/(protected)/cervejas/page.tsx
@@ -1,5 +1,6 @@
 import { cn } from '@/lib/utils'
 import { getAllCervejas } from '@/data/cervejas'
+import Link from 'next/link'
 export default async function Cervejas() {
   const listaDeCervejas = await getAllCervejas()
   if (!!listaDeCervejas?.length)
@@ -8,8 +9,20 @@ export default async function Cervejas() {
         <h1 className={cn('text-6xl font-semibold text-white drop-shadow-md')}>
           Barzim
         </h1>
-
-        <div>{JSON.stringify(listaDeCervejas)}</div>
+        <ul>
+          {listaDeCervejas.map((cerveja) => {
+            return (
+              <li key={cerveja.id}>
+                <h2>
+                  <Link href={`/cervejas/${cerveja.id}`}>
+                    {cerveja.nomeCerveja}
+                  </Link>
+                </h2>
+              </li>
+            )
+          })}
+        </ul>
+        {/* <div>{JSON.stringify(listaDeCervejas)}</div> */}
       </div>
     )
   else return <h1>oops, faltou cerveja</h1>
