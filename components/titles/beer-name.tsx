45f1047493900c1387671cd3eb6ab20387b912c8
Evandro Reichert <evandroreichert07@gmail.com>
2024-02-14 15:00:42 -0300

    ✨ 5.015: feat: adiciona componente beer-name-lg

--- components/titles/beer-name.tsx
+++ components/titles/beer-name.tsx
@@ -1,18 +1,12 @@
-import { Roboto } from 'next/font/google';
-const font = Roboto({
-    subsets: ['latin'],
-    weight: ['400', '500', '700'],
-})
-
-interface BeerProps {
+export interface BeerProps {
     cerveja: {
-        nomeCerveja: string;
-        tipoCerveja: string;
+        nomeCerveja: string
+        tipoCerveja: string
     }
     variant: 'light-mode' | 'dark-mode'
 }
 
-const getThemeByVariant = (variant: BeerProps['variant']) => {
+export const getThemeByVariant = (variant: BeerProps['variant']) => {
     switch (variant) {
         case 'light-mode':
             return 'text-black'
@@ -21,7 +15,7 @@ const getThemeByVariant = (variant: BeerProps['variant']) => {
     }
 }
 
-const normalizeTitleCase = (beer: string) => {
+export const normalizeTitleCase = (beer: string) => {
     return beer.toLowerCase().replace(/(?:^|\s)\w/g, (match) => {
         return match.toUpperCase()
     })
@@ -34,7 +28,7 @@ export const BeerName: React.FC<BeerProps> = ({ cerveja, variant }) => {
     const tipoCervejaNormalizado = normalizeTitleCase(tipoCerveja)
 
     return (
-        <div className={`${getThemeByVariant(variant)} text-left ${font.className} p-2`}>
+        <div className={`${getThemeByVariant(variant)} text-left p-2`}>
             <p className="text-[12px] opacity-60">{tipoCervejaNormalizado}</p>
             <p className='font-medium'>{nomeCervejaNormalizado}</p>
         </div>
