05532bb12886ace29b72e9f1fb8b85baaac12de5
Evandro Reichert <evandroreichert07@gmail.com>
2024-02-14 14:35:07 -0300

    ✨ 5.020: feat: adiciona componente beer-image-wrapper

--- app/war-room/page.tsx
+++ app/war-room/page.tsx
@@ -2,18 +2,21 @@ import { AddtoListButton } from "@/components/buttons/add-to-list-button"
 import { BrindarButton } from "@/components/buttons/brindar-button"
 import { BrindarReviewButton } from "@/components/buttons/brindar-review-button"
 import { LogoCervejaria } from "@/components/logos/logo-cervejarias"
+import { BeerImage } from "@/components/wrappers/beer-image-wrapper"
 import { ButtonsWrapper } from "@/components/wrappers/buttons-wrapper"
 
 export default function War() {
 
   return (<div className='bg-black-radial-gradient bg-cover h-svh flex items-center justify-center gap-10'>
-    <LogoCervejaria src="https://res.cloudinary.com/dvprux49g/image/upload/v1707930874/efz0qklqxbn84ugvhro3.png" width={100} height={90} />
+    <LogoCervejaria src="https://res.cloudinary.com/dvprux49g/image/upload/v1707930874/efz0qklqxbn84ugvhro3.png" />
+
+    <BeerImage src="https://res.cloudinary.com/dvprux49g/image/upload/v1707497542/tjyxr8vuqy9qx9sjrgsb.png" />
 
     <ButtonsWrapper>
       <BrindarButton />
       <AddtoListButton />
       <BrindarReviewButton />
     </ButtonsWrapper>
-  </div>
+  </div >
   )
 }
