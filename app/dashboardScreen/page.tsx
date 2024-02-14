ede18d9dcc57b221805f155013a5d3ad81878ea6
Lara <larabernspereira@gmail.com>
2024-02-14 20:30:12 -0300

    🤔 5.028 - Comp ninguém avaliou

--- app/dashboardScreen/page.tsx
+++ app/dashboardScreen/page.tsx
@@ -3,6 +3,7 @@ import NavWrapper from '../../components/dashboard/nav-wrapper/nav-wrapper';
 import styles from "./page.module.css"
 import BeersCarousel from '@/components/dashboard/beers-carousel/beers-carousel';
 import ListFindings from '@/components/dashboard/list-where-you-find/list-where-you-find';
+import NinguemAvaliou from '@/components/cards/ninguem-avaliou/ninguem-avaliou';
 
 const DashboardPage: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const DashboardPage: React.FC = () => {
       <Carousel/>
       <BeersCarousel/>
       <ListFindings/>
+      <NinguemAvaliou cervejaId='2'/>
     </div>
   );
 };
