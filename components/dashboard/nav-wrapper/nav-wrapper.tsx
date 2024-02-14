e3675530da7905b3f453620e22a4741e97877937
Evandro Reichert <evandroreichert07@gmail.com>
2024-02-14 14:21:25 -0300

    🚚 5.014: chore: move componente logo-barzim e corrige importações do mesmo

--- components/dashboard/nav-wrapper/nav-wrapper.tsx
+++ components/dashboard/nav-wrapper/nav-wrapper.tsx
@@ -1,26 +1,26 @@
-import styles from './nav-wrapper.module.css';
+import styles from './nav-wrapper.module.css'
 // import logoImage from '../../assets/img-logos-barzim/000 BLACK (1).png';
 // import iconPlus from "../../assets/icons/plus.png";
 // import iconNotification from "../../assets/icons/notification.png";
 // import LogoBarzimNavWrapper from './nav-wrapper-logo-barzim/nav-wrapper-logo-barzim';
 // import NavWrapperIcon from './nav-wrapper-icons/nav-wrapper-icons';
-import {HiMiniBell} from 'react-icons/hi2'
+import { HiMiniBell } from 'react-icons/hi2'
 
-import Link from 'next/link';
+import Link from 'next/link'
 
-import { Logo } from '@/components/logo';
+import { Logo } from '@/components/logos/logo-barzim'
 
 const NavWrapper: React.FC = () => {
   return (
     <div className={styles.navWrapper}>
       <Link href={'/dashboard'}>
-      <Logo width={120}/>
+        <Logo width={120} />
       </Link>
       <div className={styles.icons}>
         <HiMiniBell className="text-3xl" />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default NavWrapper;
+export default NavWrapper
