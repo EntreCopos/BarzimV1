ac3069878a5d5e6f97fc1d4dc46536e0283655c4
Evandro Reichert <evandroreichert07@gmail.com>
2024-02-15 14:29:40 -0300

    ✨ 5.024: feat: testa componentes review em /war-room

--- app/war-room/page.tsx
+++ app/war-room/page.tsx
@@ -1,12 +1,17 @@
+import AvatarReview from "@/components/avatar/avatar-review/avatar-review"
 import { AddtoListButton } from "@/components/buttons/add-to-list-button"
 import { BrindarButton } from "@/components/buttons/brindar-button"
 import { BrindarReviewButton } from "@/components/buttons/brindar-review-button"
 import { LogoCervejaria } from "@/components/logos/logo-cervejarias"
+import ReviewHeader from "@/components/review/review-header/review-header"
+import StarReviewsMini from "@/components/stars/startsMini/stars-mini"
 import { IngredientsTag } from "@/components/tags/ingredients-tag"
 import { BeerNameLarge } from "@/components/titles/beer-name-lg"
 import { BeerDescription } from "@/components/wrappers/beer-description-wrapper"
 import { BeerImage } from "@/components/wrappers/beer-image-wrapper"
 import { ButtonsWrapper } from "@/components/wrappers/buttons-wrapper"
+import { ReviewDescription } from "@/components/wrappers/review-description-wrapper"
+import { ReviewWrapper } from "@/components/wrappers/review-wrapper"
 
 export default function War() {
 
@@ -29,17 +34,27 @@ export default function War() {
           <BeerDescription description="O processo de produção da Budweiser é diferenciado, por utilizar lascas de Beechwood (madeira especial) durante os processos de fermentação e maturação. O resultado é um cerveja de sabor único e com equilíbrio perfeito: marcante no início e suave no final." />
         </div>
       </div>
+
+      <ButtonsWrapper>
+        <BrindarButton />
+        <AddtoListButton />
+      </ButtonsWrapper>
+
       <div className=" flex gap-2">
         <IngredientsTag label="Água" />
         <IngredientsTag label="Malte" />
         <IngredientsTag label="Lúpulo" />
       </div>
-      <ButtonsWrapper>
-        <BrindarButton />
-        <AddtoListButton />
+
+      <ReviewWrapper>
+        <AvatarReview avatarSrc={'https://res.cloudinary.com/dvprux49g/image/upload/v1708016390/pirk4g0flwwfgytrd669.png'} />
+        <ReviewHeader userName="irina" beerName="Budweiser" />
+        <StarReviewsMini nota={2} />
+        <ReviewDescription description="esperava mais. Não que seja ruim, mas o sabor não me cativou. Faltou algo, sabe? Por isso, dou nota 2. Há espaço para evolução." />
         <BrindarReviewButton />
-      </ButtonsWrapper>
-    </div >
+      </ReviewWrapper>
+
+    </div>
   </div>
   )
 }
