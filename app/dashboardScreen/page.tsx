8aded74d447a30d3339fe0c7ec42bc7a4028c49b
Lara <larabernspereira@gmail.com>
2024-02-15 14:07:00 -0300

    🤔 5.013 -  Componente wrapper texto conteúdo tela-cervejas

--- app/dashboardScreen/page.tsx
+++ app/dashboardScreen/page.tsx
@@ -3,23 +3,14 @@ import NavWrapper from '../../components/dashboard/nav-wrapper/nav-wrapper';
 import styles from "./page.module.css"
 import BeersCarousel from '@/components/dashboard/beers-carousel/beers-carousel';
 import ListFindings from '@/components/dashboard/list-where-you-find/list-where-you-find';
-import AvatarReview from '@/components/avatar/avatar-review/avatar-review';
-import avatarImage from '../../components/assets/icons/Designer (11) 1.png'
-import NinguemAvaliou from '@/components/cards/ninguem-avaliou/ninguem-avaliou';
-import StarReviews from '@/components/stars/stars-reviews';
-import StarReviewsMini from '@/components/stars/startsMini/stars-mini';
-import ReviewHeader from '@/components/review/review-header/review-header';
 
 const DashboardPage: React.FC = () => {
   return (
     <div className={styles.bodyDashboard}>
       <NavWrapper />
-      <Carousel/>
-      <BeersCarousel/>
-      <ListFindings/>
-      <AvatarReview avatarSrc={avatarImage}/>
-      <NinguemAvaliou cervejaId='2'/>
-      <StarReviews nota={2} />
+      <Carousel />
+      <BeersCarousel />
+      <ListFindings />
     </div>
   );
 };
---
+++ components/wrappers/wrapper-texto-conteudo/wrapper-texto-conteudo.tsx
@@ -0,0 +1,24 @@
+
+import { LogoCervejaria } from '@/components/logos/logo-cervejarias';
+import LogoCervejariaImagem from '../../assets/logo-cervejas/Ambev - png (1).png';
+import { BeerName } from '@/components/titles/beer-name';
+import StarReviews from '@/components/stars/stars-reviews';
+import { BrindarButton } from '@/components/buttons/brindar-button';
+import { AddtoListButton } from '@/components/buttons/add-to-list-button';
+import styles from './wrapper-texto-conteudo.module.css';
+
+const WrapperTextoConteudo: React.FC = () => {
+    return (
+        <div className={styles.wrapper}>
+            <LogoCervejaria src={LogoCervejariaImagem} />
+            <BeerName cerveja={{ nomeCerveja: "Budweiser", tipoCerveja: "American Lager" }} variant="dark-mode" />
+            <StarReviews nota={2} />
+            <div className={styles.buttonsContainer}>
+                <BrindarButton /* onClick={() => console.log("Brindar button clicked")} */ />
+                <AddtoListButton /*onClick={() => console.log("Add to list button clicked")} */ />
+            </div>
+        </div>
+    );
+};
+
+export default WrapperTextoConteudo;
