4ab4282b36126937dea35330a0e6a1c971bf9e88
Andre Zorek <andrezorek@gmail.com>
2024-02-16 04:16:57 -0300

    :sparkles: 4.008: feat: conta seguidores, conta seguidos, retorna um objeto com ambos

--- data/social.ts
+++ data/social.ts
@@ -30,3 +30,47 @@ export const deleteFollow = async (followerId: string, followingId: string) => {
     },
   })
 }
+
+export const getUserIdByUsername = async (username: string) =>{
+  const userId = await db.user.findFirst({
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+    },
+  })
+  return userId
+}
+
+export const countUserFollowers = async (userName: string) => {
+  const userId = await getUserIdByUsername(userName)  
+  const userFollowersNumber = await db.userFollowing.count({
+    where: {
+      followingId: {
+        equals: userId?.id as unknown as string
+      },
+    },
+  })
+  return userFollowersNumber
+}
+
+export const countUserFollowing = async (userName: string) => {
+  const userId = await getUserIdByUsername(userName)  
+  const userFollowingsNumber = await db.userFollowing.count({
+    where: {
+      followerId: {
+        equals: userId?.id as unknown as string
+      },
+    },
+  })
+  return userFollowingsNumber
+}
+
+export const getUserMetrics = async (userName: string) => {
+  const userId = await getUserIdByUsername(userName)  
+  const userFollowingsNumber = await countUserFollowing(userId?.id as unknown as string)
+  const userFollowersNumber = await countUserFollowers(userId?.id as unknown as string)
+  
+  return { userFollowersNumber, userFollowingsNumber}
+}
