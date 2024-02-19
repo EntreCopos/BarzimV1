0e668b33bd0aa71383278af02e9f26047bc6bf84
Evandro Reichert <evandroreichert07@gmail.com>
2024-02-18 22:35:33 -0300

    :recycle: 5035: refactor: remove botão câmera e adiciona cursor-pointer

--- components/buttons/add-image-review-button.tsx
+++ components/buttons/add-image-review-button.tsx
@@ -15,7 +15,7 @@ export const AddImageButton: React.FC<ImageUpladBtn> = ({handler}) => {
 
   return (
     <div className="flex text-[#CCCCCC] relative overflow-hidden">
-      <button className="relative flex items-center justify-center rounded-l-lg bg-[#2D2D2D] bg-opacity-65 px-4 py-3">
+      <button className="relative flex items-center justify-center rounded-lg bg-[#2D2D2D] bg-opacity-65 px-4 py-3 cursor-pointer">
         <FaImage />
         <input
           style={{fontSize: '100px', position: 'absolute', left: 0, top: 0, opacity: 0}}
