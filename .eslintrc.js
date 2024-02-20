1518bc4bf6905638571639355a3f356565396929
Andre Zorek <andrezorek@gmail.com>
2024-02-20 09:25:35 -0300

    :recycle: 4.010: fix: fix form tipocerveja, altera notnull campo descr tipocerveja, altera eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,11 +8,10 @@ const config = {
   extends: [
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended-type-checked",
-    // "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
   ],
   rules: {
-    // These opinionated rules are enabled in stylistic-type-checked above.
-    // Feel free to reconfigure them to your own preference.
+
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/prefer-nullish-coalescing": "off",
