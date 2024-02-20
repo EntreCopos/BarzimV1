d4a0f6885cb257bf5e77e28e5006736b316ccda3
Andre Zorek <andrezorek@gmail.com>
2024-02-20 14:35:35 -0300

    :recycle: 6.004: refact: altera component grid cards cerveja que recebe random cerveja

--- components/cards/card-vertical-cerveja.tsx
+++ components/cards/card-vertical-cerveja.tsx
@@ -17,8 +17,8 @@ export const CardVertCerveja: React.FC<CardVertCervejaProps> = ({
   const novidade = true
 
   return (
-    <div className="cursor-pointer rounded-b-xl rounded-t-xl border-[1px] border-stroke-cervejas transition-all duration-200 hover:scale-105">
-      <div className="h-30 relative flex w-36 flex-col items-center rounded-t-lg bg-stroke-cervejas p-2">
+    <div style={{height: '100%'}} className="cursor-pointer rounded-b-xl rounded-t-xl border-[1px] border-stroke-cervejas transition-all duration-200 hover:scale-101">
+      <div style={{minHeight: '60px'}} className="relative flex w-full flex-col items-center rounded-t-lg bg-stroke-cervejas p-2">
         {novidade && (
           <span className="text-black-500 absolute left-0 top-0 rounded-ee-lg rounded-tl-lg bg-yellow-barzim p-1 text-xs font-medium">
             Novidade
@@ -26,13 +26,15 @@ export const CardVertCerveja: React.FC<CardVertCervejaProps> = ({
         )}
         <Image
           src={imagem}
-          width={48}
-          height={48}
+          width={60}
+          height={60}
           alt={nomeCerveja}
-          className="pb-1"
+          // className="pb-1"
         />
       </div>
-      {children}
+      <div style={{padding: '1rem .5rem'}}>
+        {children}
+      </div>
     </div>
   )
 }
