f1f24af94244caef409d66edd5a2ede3f17856fc
Lara <larabernspereira@gmail.com>
2024-02-23 20:48:30 -0300

    ✨ feat: 5.057 - input de pesquisa de cervejas

--- components/filters/beer-filter.tsx
+++ components/filters/beer-filter.tsx
@@ -11,17 +11,27 @@ import React, { useEffect, useState } from 'react'
 import ListaDeCervejas from '../lists/lista-cervejas'
 import { type TypeObjectCerveja } from '@/data/data'
 import { WrapperDefaultPadding } from '../wrappers/wrapper-default-padding'
+import { IoSearchSharp } from "react-icons/io5";
 
 export const BeerFilter: React.FC<{ cervejas: TypeObjectCerveja[] }> = ({
   cervejas,
 }) => {
   const [cervejasFiltradas, setCervejasFiltradas] =
-    useState<TypeObjectCerveja[]>(cervejas)
-  const [ordem, setOrdem] = useState<'asc' | 'desc'>('asc')
+    useState<TypeObjectCerveja[]>(cervejas);
+  const [ordem, setOrdem] = useState<'asc' | 'desc'>('asc');
+  const [searchText, setSearchText] = useState<string>('');
 
   useEffect(() => {
-    setCervejasFiltradas(cervejas)
-  }, [cervejas])
+    setCervejasFiltradas(cervejas);
+  }, [cervejas]);
+
+  useEffect(() => {
+    // Filtrar cervejas com base no texto de pesquisa
+    const cervejasFiltradasPorPesquisa = cervejas.filter((cerveja) =>
+      cerveja.nomeCerveja.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setCervejasFiltradas(cervejasFiltradasPorPesquisa);
+  }, [cervejas, searchText]);
 
   const ordenarCervejas = (
     fnComparacao: (a: TypeObjectCerveja, b: TypeObjectCerveja) => number
@@ -61,9 +71,31 @@ export const BeerFilter: React.FC<{ cervejas: TypeObjectCerveja[] }> = ({
 
   return (
     <WrapperDefaultPadding>
+   <div style={{ marginBottom: '12px', display: 'flex', alignItems: 'center' }}>
+      <div style={{ position: 'relative', display: 'flex', alignItems: 'center', width: '100%' }}>
+        <div style={{ position: 'absolute', left: '8px' }}>
+        <IoSearchSharp style={{ color: 'var(--marfim-barzim)' }} />
+        </div>
+        <input
+          type="text"
+          placeholder="Pesquisar cervejas..."
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+          className='border-stroke-cervejas border-[1px] pl-10 w-full' 
+          style={{
+            padding: '8px',
+            fontSize: '14px',
+            borderRadius: '4px',
+            backgroundColor: 'var(--bg-gray-cards)',
+            color: 'var(--marfim-barzim)',
+            paddingLeft: '30px',
+          }}
+        />
+      </div>
+      </div>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-        <Button variant="btnCervejariaTela" style={{ marginBottom: '12px' }}>Ordenar</Button>
+          <Button variant="btnCervejariaTela" style={{ marginBottom: '12px' }}>Ordenar</Button>
         </DropdownMenuTrigger>
 
         <DropdownMenuContent className="w-56">
