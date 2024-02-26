c8b8962a480388432508bb5d0db43ed4e76d9fd7
Elian_Oliveira <75051401+elianoliver@users.noreply.github.com>
2024-02-26 18:53:34 -0300

    :sparkles: 5.058: feat: input de pesquisa de usuarios

--- app/(protected)/usuarios/page.tsx
+++ app/(protected)/usuarios/page.tsx
@@ -1,13 +1,9 @@
 // Importando as dependências necessárias
 import { auth } from '@/auth'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { getManyUsersNotPrivate, getUserByUsername } from '@/data/user'
-import { firstTwoLetters } from '@/lib/utils'
-import Link from 'next/link'
-
-// URL para o ícone de avatar padrão
-const defaultAvatarIcon =
-  'https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg'
+import { UserFilter } from '@/components/filters/user-filter'
+import { getAllUsers } from '@/data/user'
+import { ListaDeUsuarios } from '@/components/lists/lista-usuarios' // Importando o componente ListaDeUsuarios
 
 // Função assíncrona para renderizar a página de exploração de usuários
 const ExploreUsersPage = async () => {
@@ -28,46 +24,21 @@ const ExploreUsersPage = async () => {
     })
   ) : []
 
-  // Retornando o componente JSX para renderizar a página
-  return (
-    <div className='text-white flex flex-col items-center justify-center gap-4 bg-black-radial-gradient p-6'>
-      <div className='text-center'>
-        <h1 className='text-2xl pb-3 '>Olá, <b className='text-yellow-barzim'>{session?.user.name}!</b></h1>
-        <p>Os usuários abaixo já fazem parte do Barzim 😊</p>
-      </div>
-      <div className='flex flex-col px-1'>
-        {/* Verificando se existem usuários */}
-        {!!usersWithBio &&
-          // Mapeando cada usuário para um componente Avatar
-          usersWithBio.map((user) => {
-            return (
-              <div key={user.id} className='p-2 flex items-start gap-2'>
-                {/* Link para a página de perfil do usuário */}
-                <Link href={`/usuarios/${user.username} ` ?? 'dashboard'}>
-                  <Avatar>
-                    {/* Imagem do Avatar, com fallback para o ícone padrão */}
-                    <AvatarImage src={user?.image ?? defaultAvatarIcon} className='h-12' />
-                    {/* Fallback para as duas primeiras letras do nome do usuário se a imagem não estiver disponível */}
-                    <AvatarFallback>{firstTwoLetters(user?.name)}</AvatarFallback>
-                  </Avatar>
-                </Link>
+  const listaDeUsuarios = await getAllUsers();
 
-                <div className='flex flex-col items-start'>
-                  <div>{user?.name}</div>
-                  <div className='text-xs text-gray-500 break-words'>
-                    {/* Isso aqui tá BEM gambiarra mas tá funcionando */}
-                    {user.bio == "Esse usuÃ¡rio misterioso ainda nÃ£o escreveu uma bio"
-                      ? "Esse usuário misterioso ainda não escreveu uma bio"
-                      : user.bio
-                    }
-                  </div>
-                </div>
-              </div>
-            )
-          })}
+  if (!!listaDeUsuarios?.length) {
+    // Retornando o componente JSX para renderizar a página
+    return (
+      <div className='text-white flex flex-col items-center justify-center gap-4 bg-black-radial-gradient p-6'>
+        <div className='text-center'>
+          <h1 className='text-2xl pb-3 '>Olá, <b className='text-yellow-barzim'>{session?.user.name}!</b></h1>
+          <p className='pb-4'>Os usuários abaixo já fazem parte do Barzim 😊</p>
+          <UserFilter usuarios={listaDeUsuarios}/>
+        </div>
+        {/* <ListaDeUsuarios usuarios={usersWithBio} /> */}
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default ExploreUsersPage
---
+++ components/filters/user-filter.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import React, { useEffect, useState } from 'react';
+import { IoSearchSharp } from "react-icons/io5";
+import {ListaDeUsuarios} from '../lists/lista-usuarios';
+import { WrapperDefaultPadding } from '../wrappers/wrapper-default-padding';
+import { User } from '@/data/data'; // Substitua pelo caminho correto para o tipo User
+
+export const UserFilter: React.FC<{ usuarios: User[] }> = ({ usuarios }) => {
+  const [usuariosFiltrados, setUsuariosFiltrados] = useState<User[]>(usuarios);
+  const [searchText, setSearchText] = useState<string>('');
+
+  useEffect(() => {
+    setUsuariosFiltrados(usuarios);
+  }, [usuarios]);
+
+  useEffect(() => {
+    const usuariosFiltradosPorPesquisa = usuarios.filter((usuario) =>
+      usuario.name?.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setUsuariosFiltrados(usuariosFiltradosPorPesquisa);
+  }, [usuarios, searchText]);
+
+  return (
+    <>
+      <div style={{ marginBottom: '12px', display: 'flex', alignItems: 'center' }}>
+        <div style={{ position: 'relative', display: 'flex', alignItems: 'center', width: '100%' }}>
+          <div style={{ position: 'absolute', left: '8px' }}>
+            <IoSearchSharp style={{ color: 'var(--marfim-barzim)' }} />
+          </div>
+          <input
+            type="text"
+            placeholder="Pesquisar usuários..."
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            className='border-stroke-usuarios border-[1px] pl-10 w-full'
+            style={{
+              padding: '8px',
+              fontSize: '14px',
+              borderRadius: '4px',
+              backgroundColor: 'var(--bg-gray-cards)',
+              color: 'var(--marfim-barzim)',
+              paddingLeft: '30px',
+            }}
+          />
+        </div>
+      </div>
+      <ListaDeUsuarios usuarios={usuariosFiltrados} />
+    </>
+  )
+}
