471b04cb664897c042e9ddf36c817cd3fb598b9a
Evandro Reichert <evandroreichert07@gmail.com>
2024-02-27 16:46:28 -0300

    :sparkles: 5.059: feat: adiciona icons para os devs

--- components/lists/lista-usuarios.tsx
+++ components/lists/lista-usuarios.tsx
@@ -1,33 +1,33 @@
-import React from 'react';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import Link from 'next/link';
-import { firstTwoLetters } from '@/lib/utils';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { firstTwoLetters } from '@/lib/utils'
+import Link from 'next/link'
+import React from 'react'
+import { PiCodeBold } from "react-icons/pi"
+
 
 // URL para o ícone de avatar padrão
-const defaultAvatarIcon =
-  'https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg';
+const defaultAvatarIcon = 'https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg'
+const devs = ['andre', 'larabernspereira614', 'naneto', 'elianoliveira234647', 'evandroreichert']
 
 export const ListaDeUsuarios: React.FC<{ usuarios: any[] }> = ({ usuarios }) => {
   return (
     <div className='flex flex-col px-1'>
-      {/* Verificando se existem usuários */}
       {!!usuarios &&
-        // Mapeando cada usuário para um componente Avatar
         usuarios.map((user) => {
           return (
             <div key={user.id} className='p-2 flex items-start gap-2'>
-              {/* Link para a página de perfil do usuário */}
               <Link href={`/usuarios/${user.username} ` ?? 'dashboard'}>
                 <Avatar>
-                  {/* Imagem do Avatar, com fallback para o ícone padrão */}
                   <AvatarImage src={user?.image ?? defaultAvatarIcon} className='h-12' />
-                  {/* Fallback para as duas primeiras letras do nome do usuário se a imagem não estiver disponível */}
                   <AvatarFallback>{firstTwoLetters(user?.name)}</AvatarFallback>
                 </Avatar>
               </Link>
 
               <div className='flex flex-col items-start'>
-                <div>{user?.name}</div>
+                <div className='flex items-center'>
+                  {user?.name}
+                  <span className='ml-2 text-lg text-yellow-barzim' title='Desenvolvedor'>{devs.includes(user.username) ? <PiCodeBold /> : ''}</span>
+                </div>
                 <div className='text-xs text-gray-500 break-words'>
                   {/* Isso aqui tá BEM gambiarra mas tá funcionando */}
                   {user.bio == "Esse usuÃ¡rio misterioso ainda nÃ£o escreveu uma bio"
@@ -40,5 +40,5 @@ export const ListaDeUsuarios: React.FC<{ usuarios: any[] }> = ({ usuarios }) => 
           )
         })}
     </div>
-  );
+  )
 }
